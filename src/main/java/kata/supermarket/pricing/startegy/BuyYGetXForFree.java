67d0f4c1fd8ae762ebc92e7c91f79fdaeb70f576
hadjnaasse abdeldjalil <hn.abdeldjalil@gmail.com>
2019-06-28 18:12:39 +0200

    implementing Buy X product get Y for free price strategy test

---
+++ src/main/java/kata/supermarket/pricing/startegy/BuyYGetXForFree.java
@@ -0,0 +1,28 @@
+package kata.supermarket.pricing.startegy;
+
+import kata.supermarket.core.Product;
+
+import java.math.BigDecimal;
+
+public class BuyYGetXForFree implements PriceStrategy {
+
+    private final static String STRATEGY_NAME = "X_BOUGHT_Y_OFFERED_STRATEGY";
+
+    private double quantityBought ;
+    private double quantityOffered ;
+
+    public BuyYGetXForFree(double quantityBought,double quantityOffered){
+        this.quantityBought = quantityBought;
+        this.quantityOffered = quantityOffered;
+    }
+
+    @Override
+    public String getName() {
+        return STRATEGY_NAME;
+    }
+
+    @Override
+    public BigDecimal apply(Product product, double quantity) {
+        return new BigDecimal(0);
+    }
+}
--- src/test/java/kata/supermarket/pricing/PricingServiceTest.java
+++ src/test/java/kata/supermarket/pricing/PricingServiceTest.java
@@ -6,6 +6,7 @@ import kata.supermarket.Utils.UnitUtils;
 import kata.supermarket.core.Order;
 import kata.supermarket.core.Product;
 import kata.supermarket.core.Unit;
+import kata.supermarket.pricing.startegy.BuyYGetXForFree;
 import kata.supermarket.pricing.startegy.XProductForYPriceStrategy;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeAll;
@@ -81,4 +82,38 @@ public class PricingServiceTest {
         assertThat(resultPriceForXProduct, Matchers.comparesEqualTo(YPrice));
     }
 
+    @Test
+    public void should_calculate_order_complex_price_X_product_for_Y_price_for_N_product() throws Exception {
+        //Given
+        BigDecimal YPrice = new BigDecimal(1);
+
+        double XProductQuantity = 3;
+        double NProductQuantity = XProductQuantity + 2;
+
+        product.setPrice(new BigDecimal(.5));
+        product.addStrategy(new XProductForYPriceStrategy(XProductQuantity, YPrice));
+
+        Order order = new Order();
+        order.addProduct(product, UnitUtils.convert(NProductQuantity, product.getUnit(), product.getUnit()));
+        //When
+        BigDecimal resultPriceForXProduct = pricingService.getOrderTotal(order);
+        //Then
+        assertThat(resultPriceForXProduct, Matchers.comparesEqualTo(new BigDecimal(2)));
+    }
+
+    @Test
+    public void should_calculate_order_complex_price_offer_Y_for_x_Bought() throws Exception {
+        //Given
+        Order order = new Order();
+        double quantityBought = 2;
+        double quantityOffered = 1;
+        product.setPrice(new BigDecimal(1));
+        product.addStrategy(new BuyYGetXForFree(quantityBought, quantityOffered));
+
+        order.addProduct(product, UnitUtils.convert((quantityBought + quantityOffered), product.getUnit(), product.getUnit()));
+        //When
+        BigDecimal resultPrice = pricingService.getOrderTotal(order);
+        //Then
+        assertThat(resultPrice, Matchers.comparesEqualTo(new BigDecimal(2)));
+    }
 }
